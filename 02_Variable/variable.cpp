2fb04bbec40a1a7f36d95555def9b3bd2f651e38
Areeba Iqbal <areebaiqbalit@gmail.com>
2026-01-17 18:28:40 +0500

    sum is

---
+++ 02_Variable/variable.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int num1,num2,result;
+    cout <<"enter first number:";
+    cin>>num1;
+    cout<<"enter secound number:";
+    cin>>num2;
+    result=num1+num2;
+    cout<<"sum is:"<<result;
+    return 0;
+
+}

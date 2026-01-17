051e33b0970d311798fc03e5186dcb89af84053e
Areeba Iqbal <areebaiqbalit@gmail.com>
2026-01-17 20:35:22 +0500

    assignment operator

---
+++ 04_Operators/operators.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+int main() {
+    //Arithmatic operators
+    int a=5,y=3;
+    int sum = a+b;//sum=8
+    int diff = a-b;//diff=2
+    int product=a*b;//product=15
+    int divi=a/b;//divi=1
+    int reminder=a%b;//reminder=2
+    //Assignment operator
+    a=a+1;//6
+    a-=1;//4
+    a*=1;//5
+    a/=1;//5
+    a%=1;//0
+    //Comparison operator
+    bool eq = a ==b;//false
+    //cout<<eq;
+    bool t=a<b;//1t=false
+    bool gt=a>b;//gt=true
+    bool teq =a<=b;//false
+    bool gteq=a>=b;//false
+    cout<<gteq;
+    //Logical operator
+    bool a=true,b=false,c=true;
+    bool and result =a&&b;//false
+    bool orResult=a||b;//true
+    bool notResult=!a//false
+   return 0;
+
+}

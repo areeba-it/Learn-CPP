5fc93f7f5fd716e8107af1518510b9be067e5503
Areeba Iqbal <areebaiqbalit@gmail.com>
2026-01-21 23:30:44 +0500

    number

---
+++ 06_Iteration statment/for loop.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int num;
+    cout<<"Enter a Number :";
+    for(int i=1;i<5;i++){
+        cin>>num;
+    }
+    cout<<"you entered"<<num<<endl;
+    return 0;
+}

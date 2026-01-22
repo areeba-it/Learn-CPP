ad342fe5ae489a947307420054a168559bf56ef7
Areeba Iqbal <areebaiqbalit@gmail.com>
2026-01-22 19:07:11 +0500

    array

---
+++ 08_Arrays/array.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main () {
+    string student_marks[4]={"Math 80","Computer 85","Urdu 95","Bio 90"};
+    cout<<student_marks[3];
+    for(int i=0;i<4;i++){
+        cout<<student_marks[4]<<endl;
+    }
+    return 0;
+}

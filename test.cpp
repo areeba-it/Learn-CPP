c1be493523b75998650f16a96617444ce3b05069
Areeba Iqbal <areebaiqbalit@gmail.com>
2026-02-11 11:11:58 +0500

    character

---
+++ test.cpp
@@ -0,0 +1,22 @@
+//1
+//12
+//123
+//1234
+//12345
+
+
+
+#include<iostream>
+using namespace std;
+int main(){
+    int i,j;
+    char ch ='A';
+    for(i=1;i<=5;i++){
+        for(j=1;j<=i;j++){
+            cout<<ch;
+        }
+            cout<<endl;
+            ch++;
+    }
+    return 0;
+}
